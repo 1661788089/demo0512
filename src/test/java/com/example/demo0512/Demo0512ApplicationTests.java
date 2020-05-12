3765204453ffb9e1bbc8e5ec37037e9ae6cd02e8
zhangjc8 <12345678>
2020-05-12 20:22:41 +0800

    init

---
+++ src/test/java/com/example/demo0512/Demo0512ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0512;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0512ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
